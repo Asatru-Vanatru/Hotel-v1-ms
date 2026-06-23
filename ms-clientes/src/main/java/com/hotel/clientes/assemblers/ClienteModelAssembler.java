1e5e0395c0472f3d6e2663285f16913551ea718e
Raul-b7 <rau.barraza@duocuc.cl>
2026-06-23 12:15:33 -0400

    avance controllerV2 y assembler en cliente para HATEOAS

--- ms-clientes/src/main/java/com/hotel/clientes/assemblers/ClienteModelAssembler.java
+++ ms-clientes/src/main/java/com/hotel/clientes/assemblers/ClienteModelAssembler.java
@@ -1,5 +1,24 @@
 package com.hotel.clientes.assemblers;
 
-public class ClienteModelAssembler {
+import org.springframework.stereotype.Component;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 
-}
+import com.hotel.clientes.controller.ClienteControllerV2;
+import com.hotel.clientes.model.Cliente;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+@Component
+public class ClienteModelAssembler implements RepresentationModelAssembler<Cliente, EntityModel<Cliente>> {
+
+    @Override
+    public EntityModel<Cliente> toModel(Cliente cliente) {
+        return EntityModel.of(cliente,
+                linkTo(methodOn(ClienteControllerV2.class).obtenerPorId(cliente.getId())).withSelfRel(),
+                linkTo(methodOn(ClienteControllerV2.class).obtenerTodos()).withRel("clientes"));
+
+
+    }
+}    
--- ms-clientes/src/main/java/com/hotel/clientes/controller/ClienteControllerV2.java
+++ ms-clientes/src/main/java/com/hotel/clientes/controller/ClienteControllerV2.java
@@ -1,17 +1,58 @@
 package com.hotel.clientes.controller;
 
+import com.hotel.clientes.service.ClienteService;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
-@RequestMapping ("/api/v2/clientes")
+@RequestMapping("/api/v2/clientes")
 public class ClienteControllerV2 {
 
     @Autowired
-    private ServicioService servicioService;
+    private ClienteService clienteService;
 
     @Autowired
-    private ClienteModelAssembler assembler;
+    public ClienteModelAssemblers assembler;
+
+    @GetMapping(produces = MediaTypes.Hal_JSON_VALUE)
+    public CollectionModel<EntityModel<Cliente>> obtenerTodos() {
+        List<EntityModel<Cliente>> clientes = clienteService.obtenerTodos().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(clientes,
+                linkTo(methodOn(ClienteControllerV2.class).obtenerTodos()).withSelfRel()
+        );
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Cliente> obtenerPorId(@PathVariable Long id) {
+        Cliente cliente = clienteService.obtenerPorId(id);
+        return assembler.toModel(cliente);
+    }
+
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntityModel<EntityModel<Cliente>> crearCliente(@RequestBody Cliente cliente) {
+        Cliente nuevoCliente = clienteService.save(cliente);
+        return ResponseEntity
+                .created(linkTo(methodOn(ClienteControllerV2.class).obtenerPorId(nuevoCliente.getId())).toUri())
+                .body(assembler.toModel(nuevoCliente));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntityModel<EntityModel<Cliente>> actualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        Cliente clienteActualizado = clienteService.save(cliente);
+        return ResponseEntity
+                .ok(assembler.toModel(clienteActualizado));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<?> eliminarCliente(@PathVariable Long id) {
+        clienteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
+
