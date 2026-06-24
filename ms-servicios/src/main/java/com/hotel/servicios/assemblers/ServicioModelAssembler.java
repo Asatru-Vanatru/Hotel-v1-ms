45f3104cf884f6219e852ade0bf949e67e09a220
Fernando Arce <fer.arce@duocuc.cl>
2026-06-24 00:12:58 -0400

    Use ServicioResponseDTO in model assembler
    Replace Servicio domain type with ServicioResponseDTO in ServicioModelAssembler so HATEOAS EntityModel is built from the response DTO. Updated import and generic/method signatures accordingly. Compiled class artifacts under target were also changed to match the source update.

--- ms-servicios/src/main/java/com/hotel/servicios/assemblers/ServicioModelAssembler.java
+++ ms-servicios/src/main/java/com/hotel/servicios/assemblers/ServicioModelAssembler.java
@@ -3,16 +3,16 @@ package com.hotel.servicios.assemblers;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import com.hotel.servicios.controller.ServicioControllerV2;
-import com.hotel.servicios.model.Servicio;
+import com.hotel.servicios.dto.ServicioResponseDTO;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ServicioModelAssembler implements RepresentationModelAssembler<Servicio, EntityModel<Servicio>> {
+public class ServicioModelAssembler implements RepresentationModelAssembler<ServicioResponseDTO, EntityModel<ServicioResponseDTO>> {
 
     @Override
-    public EntityModel<Servicio> toModel(Servicio servicio) {
+    public EntityModel<ServicioResponseDTO> toModel(ServicioResponseDTO servicio) {
         return EntityModel.of(servicio,
                 linkTo(methodOn(ServicioControllerV2.class).getServicioById(servicio.getId())).withSelfRel(),
                 linkTo(methodOn(ServicioControllerV2.class).getAllServicios()).withRel("servicios"));
